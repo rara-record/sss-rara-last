81a3f2d1a3092503f57ace17a8b657e8fcafec93
BORA <70184893+rara-record@users.noreply.github.com>
2024-05-03 18:04:07 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { lazy, Suspense, useEffect, useState } from 'react'
-import { Wedding } from '@models/wedding'
 import FullScreenMessage from '@components/shared/FullScrrenMessage'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
@@ -12,51 +11,20 @@ const ImageViewer = lazy(() => import('@components/sections/ImageViewer'))
 const MapView = lazy(() => import('@components/sections/MapView'))
 const Account = lazy(() => import('@components/sections/Account'))
 function App() {
-  const [wedding, setWedding] = useState<Wedding | null>(null)
-  const [loading, setLoading] = useState(false)
-  const [longLoading, setLongLoading] = useState(false)
-  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   const location = useLocation()
 
   useEffect(() => {
-    setLoading(true)
-    setLongLoading(false) // 초기화
-
-    const timer = setTimeout(() => {
-      setLongLoading(true) //
+    const timeout = setTimeout(() => {
+      setLoading(false)
     }, 2000)
 
-    fetch(import.meta.env.VITE_APP_SERVER_URL_KEY)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('청첩장 정보를 불러오지 못했습니다.')
-        }
-        return response.json()
-      })
-      .then((data) => {
-        setWedding(data)
-      })
-      .catch((e) => {
-        console.error('에러발생', e)
-        setError(true)
-      })
-      .finally(() => {
-        setLoading(false)
-        clearTimeout(timer) // 로딩이 끝나면 타이머 취소
-      })
+    return () => clearTimeout(timeout)
   }, [])
 
   if (loading) {
-    return <FullScreenMessage type="loading" longLoading={longLoading} />
-  }
-
-  if (error) {
-    return <FullScreenMessage type="error" />
-  }
-
-  if (wedding == null) {
-    return null
+    return <FullScreenMessage type="loading" />
   }
 
   return (
@@ -65,32 +33,11 @@ function App() {
         <Routes location={location} key={location.pathname}>
           <Route index element={<Home />} />
           <Route path="gallery" element={<ImageGallery />} />
-          <Route
-            path="location"
-            element={<LocationInfo hallImages={wedding.hallImages} />}
-          />
-          <Route
-            path="contact"
-            element={<Contact groom={wedding.groom} bride={wedding.bride} />}
-          />
-          <Route
-            path="/gallery/image-viewer"
-            element={<ImageViewer images={wedding.galleryImages} />}
-          />
-          <Route
-            path="/location/map-viewer"
-            element={<MapView location={wedding.location} />}
-          />
-          <Route
-            path="/contact/account-viewer"
-            element={
-              <Account
-                groom={wedding.groom}
-                bride={wedding.bride}
-                date={wedding.date}
-              />
-            }
-          />
+          <Route path="location" element={<LocationInfo />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="/gallery/image-viewer" element={<ImageViewer />} />
+          <Route path="/location/map-viewer" element={<MapView />} />
+          <Route path="/contact/account-viewer" element={<Account />} />
         </Routes>
       </Suspense>
     </AnimatePresence>
