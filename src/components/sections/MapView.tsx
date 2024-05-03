55e5c129639e89ff2627b8a406e4462c36ca7030
BORA <70184893+rara-record@users.noreply.github.com>
2024-05-03 18:02:41 +0900

    Update MapView.tsx

--- src/components/sections/MapView.tsx
+++ src/components/sections/MapView.tsx
@@ -2,7 +2,6 @@ import { ReactNode, useEffect, useRef } from 'react'
 import Section from '@shared/Section'
 import styles from './MapView.module.scss'
 import classNames from 'classnames/bind'
-import { Location } from '@models/wedding'
 import { Link } from 'react-router-dom'
 import kakao_navigation from '/assets/images/kakao_navigation.svg'
 import naver_navigation from '/assets/images/naver_navigation.webp'
@@ -11,6 +10,8 @@ import { IoMdArrowRoundBack } from '@react-icons/all-files/io/IoMdArrowRoundBack
 import { FaCar } from '@react-icons/all-files/fa/FaCar'
 import { FaBus } from '@react-icons/all-files/fa/FaBus'
 import { IoIosSubway } from '@react-icons/all-files/io/IoIosSubway'
+import { IoIosCopy } from '@react-icons/all-files/io/IoIosCopy'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 const cx = classNames.bind(styles)
 
@@ -20,7 +21,23 @@ declare global {
   }
 }
 
-const MapView = ({ location }: { location: Location }) => {
+const location = {
+  lat: 37.5672668,
+  lng: 126.8269593,
+  name: '보타닉파크웨딩 오키드홀',
+  address:
+    '서울시 강서구 마곡중앙5로 6 보타닉 푸르지오시티 로비층 보타닉파크 웨딩',
+  kakao_link:
+    'https://map.kakao.com/?map_type=TYPE_MAP&target=car&rt=%2C%2C461732%2C1129923&rt1=&rt2=%EB%B3%B4%ED%83%80%EB%8B%89%ED%8C%8C%ED%81%AC%EC%9B%A8%EB%94%A9&rtIds=%2C1089036510&rtTypes=%2CPLACE',
+  naver_link: 'https://map.naver.com/p/entry/place/94306527?c=15.00,0,0,0,dh',
+  waytocome: {
+    metro: ['9호선/공항철도 - 마곡나루역 1, 2번 출구방면 진입통로 연결'],
+    bus: ['마곡나루역 정류장 하차'],
+    car: ['주차장 안내: 지하 3층~지하 8층 주차 가능'],
+  },
+}
+
+const MapView = () => {
   const mapContainer = useRef(null)
   useEffect(() => {
     const script = document.createElement('script')
@@ -67,6 +84,20 @@ const MapView = ({ location }: { location: Location }) => {
           </div>
           <div className={cx('wrap-map')}>
             <div className={cx('map')} ref={mapContainer}></div>
+          </div>
+
+          <div className={cx('address')}>
+            서울시 강서구 마곡중앙5로 6 (서울시 강서구 마곡동 760)
+            <CopyToClipboard
+              text={`서울시 강서구 마곡중앙5로 6 (서울시 강서구 마곡동760 )`}
+              onCopy={() => {
+                alert('복사가 완료되었습니다.')
+              }}
+            >
+              <button type="button">
+                <IoIosCopy />
+              </button>
+            </CopyToClipboard>
           </div>
 
           <div className={cx('come-wrapper')}>
